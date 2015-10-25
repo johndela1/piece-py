55065de80d839c9d81e0602460f284458370f9a0
John de la Garza <john@jjdev.com>
2015-10-25 15:38:53 -0700

    add flow for client results stored and returned

--- server.py
+++ server.py
@@ -16,17 +16,17 @@ def get_deltas(pattern_name=None, bpm=None):
 	
 
 @app.route('/attempt', methods=['POST'])
-def set_attempt():
-	pattern_name = request.json['pattern_name']
-	bpm = request.json['bpm']
-	in_tss = request.json['tss']
-	player = request.json['player']
+def post_attempt():
+	pattern_name = request.values['pattern_name']
+	bpm = int(request.values['bpm'])
+	in_tss = [int(x) for x in request.values['tss'].split(',')]
+	player = request.values['player']
 
 	pattern = db.get_pattern(pattern_name)
 	ref_tss = pattern_tss(pattern, bpm)
 	res = result(ref_tss, in_tss)
 	db.write(res, pattern_name, player, bpm)
-	return jsonify(res)
+	return str(res)+'<br><a href=http://127.0.0.1:5000/static/client.html>back</a>'
 
 if __name__ == '__main__':
 	app.run(debug=True)
