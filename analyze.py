03e87fc40d9a3cbb971436bfc32b6c31a5065f15
John de la Garza <john@jjdev.com>
2015-11-10 23:03:05 -0800

    analyze: add trial function
    This function takes two sequences, reference and user input.  It returns
    a Bool, pass or fail.

--- analyze.py
+++ analyze.py
@@ -73,6 +73,11 @@ def analysis(tss_ref, tss_in):
 	else:
 		return [(sample, None)] + analysis(samples, tss_in)
 
+def trial(tss_ref, tss_in):
+    return (len(tss_ref) == len(tss_in) and
+        not any(map(lambda x: abs(x) > TOLERANCE,
+                [ts_ref - ts_in for ts_ref, ts_in in zip(tss_ref, tss_in)])))
+
 def reshape(analysis):
 	ret = {}
 	ret['delta'] = []
@@ -89,7 +94,6 @@ def reshape(analysis):
 			ret['extra'].append(i)
 	return ret
 
-		
 ##########################################
 
 assert (deltas_tss([0, 500, 500]) == i_deltas_tss([0, 500, 500]) ==
@@ -97,5 +101,33 @@ assert (deltas_tss([0, 500, 500]) == i_deltas_tss([0, 500, 500]) ==
 assert pattern_deltas((4, [1, 1, 1, 1]), 60) == [0, 1000, 1000, 1000]
 analysis([0,100,200], [0, 101, 300])
 
+def cmp(x, y):
+    return x[0]*x[1] - y[0]*y[1]
+
+def easier(pattern, patterns):
+    low = 0
+    hi = len(patterns) - 1
+    while low < hi:
+        mid = (low+hi) >> 1
+        delta = cmp(pattern, patterns[mid])
+        if delta == 0:
+            return patterns[mid >> 1]
+        elif delta < 0:
+            hi = mid
+        else:
+            low = mid+1
+
 if __name__ == '__main__':
-	print(reshape(analysis([0,100,200], [0, 101, 300])))
+	#print(reshape(analysis([0,100,200], [0, 101, 300])))
+    tss_ref=[0,500,1000]
+    tss_in=[10,450,1300]
+    print(trial(tss_ref, tss_in))
+    tss_in=[0,500,1080]
+    print(trial(tss_ref, tss_in))
+
+
+    patterns = [(div, bpm, int(div/4)) for div in (4,8) for bpm in range(60,121,10)]
+    print (str(dict(patterns = patterns)))
+    tss_in=(4,100,1)
+    print('input:', tss_in)
+    print(easier((4,100,1), patterns))
