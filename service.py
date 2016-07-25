d6ad61cc36f3bab868278dd8431fc36a85747f24
John de la Garza <john@jjdev.com>
2016-07-24 19:18:42 -0700

    service: fix write to start working

--- service.py
+++ service.py
@@ -2,33 +2,39 @@ from itertools import chain
 
 from analyze import p_tss, p_dts
 import analyze
-from db2 import Delta, Extra, Miss, session, Pattern
+from db2 import Delta, Extra, Miss, session, Pattern, Attempt
 
 
-def write(a):
-    attempt = Attempt(
-            deltas=[Delta(ts=ts, val=val) for ts, val in a['deltas']],
-            extras=[Extra(ts=ts) for ts in a['extras']],
-            misses=[Miss(ts=ts) for ts in a['misses']],
+def write(analysis, name, bpm):
+    session.add(
+        Attempt(
+            deltas=[Delta(ts=ts, val=val) for ts, val in analysis['deltas']],
+            extras=[Extra(ts=ts) for ts in analysis['extras']],
+            misses=[Miss(ts=ts) for ts in analysis['misses']],
+            pattern_id=session.query(Pattern).filter_by(name=name).one().id,
+            bpm=bpm,
             )
-    session.add(attempt)
-    session.flush()
+        )
+    session.commit()
 
 
 def deltas_with_note_count(name, bpm):
-    pattern = session.query(Pattern).filter_by(name=name).one()
-    return (p_dts(((pattern.beats, pattern.beat_unit), eval(pattern.notes)), bpm),
-            len(list(chain(*(eval(pattern.notes))))))
+    p = session.query(Pattern).filter_by(name=name).one()
+    return (p_dts(((p.beats, p.beat_unit), eval(p.notes)), bpm),
+            len(list(chain(*(eval(p.notes))))))
 
 
 def submit(name, bpm, tss_in):
     pattern = session.query(Pattern).filter_by(name=name).one()
     tss_ref = p_tss(((pattern.beats, pattern.beat_unit), eval(pattern.notes)), bpm)
-    return analyze.analysis(tss_ref, tss_in)
+    analysis = analyze.analysis(tss_ref, tss_in)
+    write(analysis, name, bpm)
+    print(analysis)
 
 
-#session.add(Pattern(name='easy-4', beats=4, beat_unit=4, notes='[[1], [1], [1], [1]]'))
-#session.add(Pattern(name='2-3', beats=4, beat_unit=4, notes='[[1,1], [1,1,1]]'))
-#session.commit()
+if not session.query(Pattern).first():
+    session.add(Pattern(name='easy-4', beats=4, beat_unit=4, notes='[[1], [1], [1], [1]]'))
+    session.add(Pattern(name='2-3', beats=4, beat_unit=4, notes='[[1,1], [1,1,1]]'))
+    session.commit()
 if __name__ == '__main__':
     import pdb;pdb.set_trace()
