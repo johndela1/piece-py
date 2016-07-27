e545363b2c77f6790f9a9c181d4b1b4ea29be2a1
John de la Garza <john@jjdev.com>
2016-07-26 20:48:51 -0700

    cli: refactor get_tss_in

--- cli.py
+++ cli.py
@@ -7,18 +7,19 @@ PRE_SCALER = 1000
 
 def get_tss_in(n):
     dev = InputDevice('/dev/input/event8')
-    x = 0
     base = None
-    tss = []
+    tss_in = []
+    x = 0
     for e in dev.read_loop():
+        if not(e.type == ecodes.EV_KEY and e.value == 1):
+            continue
+        if base is None:
+            base = e.timestamp()
+        tss_in.append(int((e.timestamp()-base)*PRE_SCALER))
+        x += 1
         if x >= n:
             break
-        if e.type == ecodes.EV_KEY and e.value == 1:
-            if base == None:
-                base = e.timestamp()
-            x += 1
-            tss.append(int((e.timestamp()-base)*PRE_SCALER))
-    return tss
+    return tss_in
 
 def play(deltas):
     for dt in deltas:
@@ -30,9 +31,12 @@ if __name__ == '__main__':
     name = 'easy-4'
     # name = '2-3'
     bpm = 120
-    deltas, note_count = service.deltas_with_note_count(name=name, bpm=bpm)
 
+    deltas, note_count = service.deltas_with_note_count(name=name, bpm=bpm)
     play(deltas)
     tss_in = get_tss_in(note_count)
 
-    print(service.submit(name, bpm, tss_in))
+    analysis = service.submit(name, bpm, tss_in)
+
+    print('result:', analysis['result'])
+    print(analysis)
